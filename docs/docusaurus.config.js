75ffc273b02a0d441c548674fcaba2dd4fe87d7a
Pavel Strashnov <pavel.strashnov@getmoss.com>
2022-12-27 09:53:27 +0100

    Update config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Moss UI',
+  tagline: 'Design language we speak',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Moss UI',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -67,7 +67,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Components',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
