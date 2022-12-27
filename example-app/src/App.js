e2cd3a7d2489c54dd67b14e231edf6a664126d73
Pavel Strashnov <pavel.strashnov@getmoss.com>
2022-12-27 08:13:05 +0100

    Add workspace

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@moss-ui/components'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Button>Hello, components!</Button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
