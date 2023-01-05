846537ee5d5dc97acbc6fee94f060dba7e78fe54
Pavel Strashnov <pavel.strashnov@getmoss.com>
2023-01-05 09:27:10 +0100

    Remove sass

--- components/webpack.config.js
+++ components/webpack.config.js
@@ -23,15 +23,6 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.sass$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        // include: path.resolve(__dirname, './src'),
-      },
     ],
   },
   resolve: {
